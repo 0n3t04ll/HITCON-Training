63aaa4b7d4261aefb20b7774724cf0c25ccf22df
Azeria-Lab-v1 <b80628@gmail.com>
2019-05-18 09:05:38 -0700

    move git clone cmd to another script

---
+++ git_repo_setup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+git clone https://github.com/scwuaptx/peda.git ~/peda 
+cp ~/peda/.inputrc ~/
+git clone https://github.com/scwuaptx/Pwngdb.git
+cp ~/Pwngdb/.gdbinit ~/
+git clone https://github.com/JonathanSalwan/ROPgadget
